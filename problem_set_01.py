3d2e5d866e57b1a7a9e73edb80b9324223935ae9
Aspen Hollyer <aspen.dollins@gmail.com>
2017-03-30 07:12:37 -0500

    Optimize isPrime to test only necessary values

--- problem_set_01.py
+++ problem_set_01.py
@@ -10,7 +10,9 @@ Created on Mon Mar 13 09:04:41 2017
 import math
 
 def isPrime(n):
-    for i in range(2, n):
+    #accepts an integer, tests whether it is prime,
+    #and returns a boolean
+    for i in range(2, math.ceil(n/2)):
         if n % i == 0:
             return False
     return True;
@@ -18,10 +20,10 @@ def isPrime(n):
 primeCounter = 1 #because 2 is prime
 x = 3
 
-while primeCounter < 999:
+while primeCounter < 1000:
     if isPrime(x):
         primeCounter += 1
         print(x, "is prime number", primeCounter)
     x += 2 #increment x by 2 for the next odd number
     
-print(x, "is the 1000th prime number!")
+print(x-2, "is the 1000th prime number!")

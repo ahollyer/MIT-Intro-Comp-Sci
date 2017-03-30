d0f720a00d6a7fe616582f678b9aa8f59da34504
Aspen Hollyer <aspen.dollins@gmail.com>
2017-03-30 07:15:21 -0500

    Add specification

--- is-between.py
+++ is-between.py
@@ -7,6 +7,8 @@ Created on Sun Mar 19 11:28:03 2017
 """
 
 def isBetween(x, lowerBound, upperBound):
+    #accepts a number, a lower bound, and an upper bound, tests whether the
+    #number falls within the bounds, and returns a boolean
     return lowerBound < x < upperBound
 
 print(isBetween(3, 1, 5))
